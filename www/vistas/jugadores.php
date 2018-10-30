e1f65f872ebef0040364053be1ab32d2d51be625
jovianri <jovianri@hotmail.com>
2018-10-30 09:56:50 +0100

    jugadores OK

--- www/vistas/jugadores.php
+++ www/vistas/jugadores.php
@@ -6,42 +6,23 @@
     <link rel="stylesheet" type="text/css" media="screen" href="<?=CSS . DS?>estilos.css" />
 </head>
 <body>
-<?php
+    <?php
     include "menu.php";
-?>
-<div id="content">
-    <div id="players">        
-        <a href="/jugador/1">
-            <figure class="playersTeam">
-                <img src="<?= IMAGENES . DS?>lonzo-ball.jpg" />
-                <figcaption>Lonzo Ball</figcaption>
-            </figure>
-        </a>
-        <a href="/jugador/2">
-            <figure class="playersTeam">
-            <img src="<?= IMAGENES . DS?>lebron-james.jpg" />
-                <figcaption>Lebron James</figcaption>
-            </figure>
-        </a>
-        <a href="/jugador/3">
-            <figure class="playersTeam">
-                <img src="<?= IMAGENES . DS?>kyle-kuzma.jpg" />
-                <figcaption>Kyle Kuzma</figcaption>
-            </figure>
-        </a>
-        <a href="/jugador/4">
-            <figure class="playersTeam">
-                <img src="<?= IMAGENES . DS?>brandon-ingram.jpg" />
-                <figcaption>Brandon Ingram</figcaption>
-            </figure>
-        </a>
-        <a href="/jugador/5">
-            <figure class="playersTeam">
-                <img src="<?= IMAGENES . DS?>javale-mcgee.jpg" />
-                <figcaption>Javale McGee</figcaption>
-            </figure>
-        </a>
+    ?>
+    <div id="content">
+        <div id="players">
+            <?php
+            $sql = 'SELECT codigo, nombre, foto FROM jugadores WHERE Nombre_equipo = "Lakers" ORDER BY nombre LIMIT 5';
+            foreach ($mysql->query($sql) as $row) {
+            echo '<a href="/jugador/"'.$row['codigo'].'>
+                <figure class="playersTeam">
+                    <img src="'.IMAGENES . DS . $row['foto'].'" style=" width: 120px; height: 180px;" />
+                    <figcaption>'.$row['nombre'].'</figcaption>
+                </figure>
+            </a>';
+        }
+        ?>
     </div>
-</div>    
+</div>
 </body>
 </html>
