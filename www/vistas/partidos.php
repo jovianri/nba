ddaa5bf4dbb6a18c169d743599b14b29fec9a78b
jovianri <jovianri@hotmail.com>
2018-11-09 10:05:04 +0100

    no he hecho nada

--- www/vistas/partidos.php
+++ www/vistas/partidos.php
@@ -13,23 +13,27 @@
 <select id="temporada" onchange="temporada()">
 <option value="0">Seleccione una temporada</option>
 <?php
+    $key = $keys[0][0];
+    $idtemporada = $params[$key];
+    $idtemporada = str_replace("-", "/", $idtemporada);
     $sql = 'SELECT MIN(codigo) AS codigo, temporada FROM partidos GROUP BY temporada ORDER BY codigo';
     foreach ($mysql->query($sql) as $row) {
-        echo '<option value="'.$row['temporada'].'">Temporada: '.$row['temporada'].'</option>';
+        if ($row['temporada'] == $idtemporada) {
+            echo '<option value="'.$row['temporada'].'" selected>Temporada: '.$row['temporada'].'</option>';
+        } else {
+            echo '<option value="'.$row['temporada'].'">Temporada: '.$row['temporada'].'</option>';
+        }
     }
 ?>
 </select>
 <script>
 function temporada(){
-var selected = document.getElementById("temporada").value;
-selected = selected.replace("/", "-");
-window.location.replace("http://nba.test/partidos/"+selected);
+    var selected = document.getElementById("temporada").value;
+    selected = selected.replace("/", "-");
+    window.location.replace("http://nba.test/partidos/"+selected);
 }
 </script>
 <?php
-$key = $keys[0][0];
-$idtemporada = $params[$key];
-$idtemporada = str_replace("-", "/", $idtemporada);
 echo "<br><h2>Temporada: ".$idtemporada."</h2><br>";
 $sql = 'SELECT * FROM partidos WHERE temporada = "'.$idtemporada.'" AND equipo_local = "Lakers" OR equipo_visitante = "Lakers"';
 foreach ($mysql->query($sql) as $row) {
